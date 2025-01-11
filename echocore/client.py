fe72a09dbed1df439249fca6575e7c726b17e67f
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-11 08:27:14 +0100

    Update client.py

--- echocore/client.py
+++ echocore/client.py
@@ -1,6 +1,18 @@
-
+import requests
 
 
 class EchoClient:
     def __init__(self, token):
         self.token = token
+
+
+    def setup_plugins(self, BOT_ID):
+        response = requests.get(f"https://testing.vercel.app/plugins={BOT_ID}")
+        if response.status_code == 200:
+            plugins_list = response["result"]
+            file_list = []
+            for plugin_id in plugins_list:
+                file_url = f"https://raw.githubusercontent.com/ishikki-akabane/EchoCore/main/{plugin_id}"
+                file_list.append(file_url)
+
+            return file_url
